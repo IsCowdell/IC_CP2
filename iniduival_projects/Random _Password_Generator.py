ddbdd77f84729850992f4e68310f9e30ee056b40
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-01-22 09:52:03 -0700

    random password generator

---
+++ iniduival_projects/Random _Password_Generator.py
@@ -0,0 +1,54 @@
+# IC 1st random password generator
+#import string 
+import string
+#import random 
+import random
+
+#defining main functions
+def main(): 
+# show the user Type the number for the action you would like to perform
+ print(" type the number for the action you would like to perform")
+#1. Generate Passwords
+#2. Exit
+# if 1 then:
+# generate password function() 
+# else if 2 then
+# exit function 
+# else:
+# show the user incorrect vaule try again
+
+
+#defining exit function 
+# show the user thank you for using my password 
+#quit program
+
+
+# defining generate password function()
+#characters = ""
+#  length = ask user How long does the password need to be: 
+
+# lower = ask the user does the password need lowercase letters (Y/N):
+#if yes then .append(string.ascii_lowercase) to characters 
+#else:
+#if no print you dont want lowercase
+# upppercase = ask the user Does the password need uppercase letters (Y/N):
+#if yes then += (string.ascii_uppercase) to characters 
+#else:
+#if no print you dont want uppercase
+# number = ask the user Does the password need numbers (Y/N):
+#if yes then += (string.digits) to characters 
+#else:
+#if no print you dont want numbers 
+
+#  special_characters = ask the user Does the password need special characters letters (Y/N):
+#if yes then += (string.punctuation) to characters 
+#else:
+#if no print you dont want special characters 
+
+
+# generate password 
+# for i in range length 
+# password plus equal random. choice(characters )
+#print password 
+
+
