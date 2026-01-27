6b712603973c978fadd1dd1f883509e623cc423f
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-01-27 04:40:16 +0000

    random password generator done done

--- iniduival_projects/Random _Password_Generator.py
+++ iniduival_projects/Random _Password_Generator.py
@@ -1,111 +1,92 @@
 # IC 1st random password generator
 #import string 
 import string
-#import random 
+#import random
 import random
 
-
-
-
-# defining generate password function()
-def generate():
-#characters = ""
+# Functions for the different password requirements
+def get_user_requirements():
+    # creating an empty string
     characters = ""
-#  length = ask user How long does the password need to be: 
-    length = input("how long does your password have to be: ")
+    
+    # length = ask user How long does the password need to be: 
+    length = int(input("How long does your password have to be: "))
 
-# lower = ask the user does the password need lowercase letters (Y/N):
-    lower = input(" does your password need lowercase yes or no: ")
-#if yes then += (string.ascii_lowercase) to characters 
-    if "yes":
+    # lower = ask the user does the password need lowercase letters (Y/N):
+    lower = input("Does your password need lowercase (yes/no): ").lower()
+    # if yes then += add lowercase to characters 
+    if lower == "yes":
         characters += string.ascii_lowercase
-#if else no print you dont want lowercase
-    elif "no":
-        print(" you dont want lowercase")
-# else:
-    else:
-# show user inccorect vaule
-        print("incorrect vaule")
-# upppercase = ask the user Does the password need uppercase letters (Y/N):
-    uppercase = input("does your password need uppercase yes or no")
-#if yes then += (string.ascii_uppercase) to characters 
-    if "yes":
+
+    # upppercase = ask the user Does the password need uppercase letters (Y/N):
+    uppercase = input("Does your password need uppercase (yes/no): ").lower()
+    # if yes then += add uppercase to characters 
+    if uppercase == "yes":
         characters += string.ascii_uppercase
-  #if else no print you dont want uppercase
-    elif "no":
-        print(" you dont want uppercase")      
-#else:
-    else:
-#show user incorrect vaule 
-        print("incorrect value")
-# number = ask the user Does the password need numbers (Y/N):
-    number = input(" do you need numbers yes or no")
-#if yes then += (string.digits) to characters 
-    if "yes":
+
+    # number = ask the user Does the password need numbers (Y/N):
+    number = input("Do you need numbers (yes/no): ").lower()
+    # if yes then += add digits to characters 
+    if number == "yes":
         characters += string.digits
-#if else no print you dont want digits
-    elif "no":
-        print(" you dont want numbers")  
-#else:
-    # show user incorrect value
-    else:
-        print(" show user incorrect value")
 
-#  special_characters = ask the user Does the password need special characters letters (Y/N):
-    special_characters = input("Does the password need special characters letters yes or no: ")
-#if yes then += (string.punctuation) to characters 
-    if "yes":
+    # special_characters = ask the user Does the password need special characters letters (Y/N):
+    special = input("Does the password need special characters (yes/no): ").lower()
+    # if yes then += add special characters to characters 
+    if special == "yes":
         characters += string.punctuation
-#if else no print you dont want characters
-    elif "no":
-        print(" you dont want special characters")  
-#else:
-    # show user incorrect value
+        
+    return length, characters
+
+# A function that makes the password once it is the correct length
+def assemble_password(length, characters):
+    options = []
+        # Loop 4 times to create four different options
+    for selection in range(4):
+            # generate password 
+            password = ""
+            # for i in range length 
+            for i in range(length):
+                # password plus equal random. choice(characters)
+                password += random.choice(characters)
+            options.append(password)
+    
+    return options
+
+# A main function that runs the code
+def main():
+    # show the user Type the number for the action you would like to perform
+    print("Password Generator")
+    print("1. Generate Password")
+    print("2. Exit")
+    #giving the user rights
+    choice = input("Type the number for the action you would like to perform: ")
+
+    # if 1 then:
+    if choice == "1":
+        # Call the requirements function
+        pass_length, char_pool = get_user_requirements()
+        
+        # Call the assembly function
+        password_list = assemble_password(pass_length, char_pool)
+        #printing the passwords in different lines
+        print("\nHere are your 4 password options")
+        for password in password_list:
+            print("-" + password)
+        
+        # Loop back to start
+        main()
+
+    # else if 2 then
+    elif choice == "2":
+        # show the user thank you for using my password 
+        print("Thank you for using my program. Goodbye!")
+    
+    # else: show user incorrect vaule try again
     else:
-        print(" show user incorrect value")
+        print("Incorrect value, try again.")
+        main()
 
-# generate password 
-# for i in range length 
-    for i in length:
-        password = ""
-# password plus equal random. choice(characters )
-        password += random.choice(characters)
-#print password 
-        print(password)
-#defining main functions
-
-def main(): 
-# show the user Type the number for the action you would like to perform
-    choice = input(f" type the number for the action you would like to perform \n 1 is for generate password \n 2 is to quit:").strip()
-#1. Generate Passwords
-#2. Exit
-# if 1 then:
-    end = False
-# msking a loop that repeats until it wants to end
-    if end == False:
-        if choice == "1":
-            generate()
-# generate password function() 
-    #generate_password()
-# else if 2 then
-        elif choice == "2":
-    #chaning variabe
-            end == True
-# exit function 
-            exit()
-# exit()
-# else:
-        else:
-# show the user incorrect vaule try again
-            print(" Incorrect vaule try again")
-
-
-#defining exit function 
-def exit():
-# show the user thank you for using my password 
-    print(" thank you for using my program good bye")
-#quit program
-    quit
-
+# Run the program
 main()
 
