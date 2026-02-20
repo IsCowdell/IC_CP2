c9d147bef5e8c58316a36038f31d163b08f7dcd7
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-20 09:52:39 -0700

    word counter

---
+++ iniduival_projects/word_counter.py
@@ -0,0 +1,35 @@
+#IC 1st worder counter 
+#You can do 4 things\
+# 1 is upadte document infor 
+#2 is to view document 
+#3 is to add content to document 
+#4 is exit the program 
+
+
+
+
+#add Doncument information would be a function
+# defing the upadting doc info 
+# with open plce file path and put the mod into "a" for append as fil:
+#   variable of writer as = csv.write(file)
+#    #write the new row
+    # wHAT DO YOU WANT TO ADD  # Ask user for input for a new row
+    # The inputs are stored in variables
+    value1 = input("Enter column 1 data: ")
+    value2 = input("Enter column 2 data: ")
+    value3 = input("Enter column 3 data: ")
+    
+    # Create a list with the new data
+    new_row = [value1, value2, value3]
+
+
+
+#viewing doc information would be a function
+# with open plce file path and put the mod into "r" for reading as fil:
+#   variable of reader as = csv.read(file)
+#    #make sure to have them read every single row
+
+
+#To exit the program 
+# show the user goodbye!
+# break out of program 
