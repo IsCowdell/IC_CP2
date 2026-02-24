4e59f84a42e95728b2fe1f571ff40846d9e78d10
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-24 09:50:20 -0700

    recursion

--- Notes/recursion.py
+++ Notes/recursion.py
@@ -16,3 +16,26 @@ def factorial(n):
     if n == 1: return 1 #base case
     return n * factorial(n-1)
 print(f"recursion:{factorial(num)}")
+
+fib = [1,1]
+
+
+for i in range(1,11): 
+    fib.append(fib[i-1] + fib[i])
+
+print(f"loop:{fib}")
+
+numbers = []
+def fibonacci(n):
+    #numbers.append(n)
+    if n == 2: 
+        return 1
+    elif n == 1:
+        return 0
+    else: 
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(f"recursion:{fibonacci(13)}")
+
+
+
