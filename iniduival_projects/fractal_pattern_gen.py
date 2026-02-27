17a459a7ffddf8853c9cbc77923298474f58ed70
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-02-27 09:52:29 -0700

    fractal pattern

---
+++ iniduival_projects/fractal_pattern_gen.py
@@ -0,0 +1,19 @@
+#IC 1st fractal pattern
+import turtle
+# Tell the user you have enter the traignle geneater
+#Enter recursion dept(1-5)
+# Intake how many depths the user wants and then take that and somehow put it into a recursion
+# to make it keep going and going 
+# Define the turtle drawing triangle
+# 
+
+
+
+# def getting mid point whic calling p1 and p2 as a parameter 
+# which then return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2) which finds the midpoint 
+#
+
+
+
+#
+
