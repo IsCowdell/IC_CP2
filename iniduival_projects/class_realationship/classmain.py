3f836a755e4289eb3b6a8493444c4cf1e35635c1
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-03-26 09:52:37 -0600

    Finished up psecucode

---
+++ iniduival_projects/class_realationship/classmain.py
@@ -0,0 +1,46 @@
+#Starting off the main menu 
+#print welcome to the class grade book 
+# what do you want to do 
+# 1 add new student 
+# 2 add grade to student 
+# 3 view student record 
+# 4 view all students 
+# 5 Class sumamary 
+# 6 exit
+# please enter your choice(1-6) Please only select one of this 
+# make an if loop to check if nu,ber is actually in range 
+#if not keep bugging them until they make vaild choice 
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for class sumary include their names,grades and class average overall
+# for the nuermical average make sure to use the data they already gave us
